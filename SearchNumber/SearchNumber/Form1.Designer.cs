1b514dcb61ee86366eeb821f426a398be90a8526
Isaque de Araujo <isaquearaujo22@gmail.com>
2016-04-02 15:04:44 -0300

    SearchNumbers
    Quase tudo feito, na verdade está pronto.

---
+++ SearchNumber/SearchNumber/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace SearchNumber
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GerarNumeros = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SearchLinear = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SearchBinary = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // GerarNumeros
+            // 
+            this.GerarNumeros.Location = new System.Drawing.Point(12, 12);
+            this.GerarNumeros.Name = "GerarNumeros";
+            this.GerarNumeros.Size = new System.Drawing.Size(121, 40);
+            this.GerarNumeros.TabIndex = 0;
+            this.GerarNumeros.Text = "Gerar 1000 Numeros Ordenados";
+            this.GerarNumeros.UseVisualStyleBackColor = true;
+            this.GerarNumeros.Click += new System.EventHandler(this.GerarNumeros_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 58);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(121, 173);
+            this.listBox1.TabIndex = 1;
+            // 
+            // SearchLinear
+            // 
+            this.SearchLinear.Location = new System.Drawing.Point(151, 12);
+            this.SearchLinear.Name = "SearchLinear";
+            this.SearchLinear.Size = new System.Drawing.Size(121, 41);
+            this.SearchLinear.TabIndex = 2;
+            this.SearchLinear.Text = "Encontar Número De Forma Linear";
+            this.SearchLinear.UseVisualStyleBackColor = true;
+            this.SearchLinear.Click += new System.EventHandler(this.SearchLinear_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(162, 59);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // SearchBinary
+            // 
+            this.SearchBinary.Location = new System.Drawing.Point(151, 85);
+            this.SearchBinary.Name = "SearchBinary";
+            this.SearchBinary.Size = new System.Drawing.Size(121, 41);
+            this.SearchBinary.TabIndex = 4;
+            this.SearchBinary.Text = "Encontar Número De Forma Binária";
+            this.SearchBinary.UseVisualStyleBackColor = true;
+            this.SearchBinary.Click += new System.EventHandler(this.SearchBinary_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.SearchBinary);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.SearchLinear);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.GerarNumeros);
+            this.Name = "Form1";
+            this.Text = "Busca Linear e Binária";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button GerarNumeros;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button SearchLinear;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button SearchBinary;
+    }
+}
+
---
+++ SearchNumber/SearchNumber/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace SearchNumber
+{
+    public partial class Form1 : Form
+    {
+        static Stopwatch stopwatch = new Stopwatch();
+        static TimeSpan ts;
+        static List<string> numbers = new List<string>();
+        int value;
+        static int contador;
+        static Random random = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void GerarNumeros_Click(object sender, EventArgs e)
+        {
+            numbers.Clear();
+            contador = numbers.Count;
+            while (contador < 1000)
+            {
+                value = random.Next(0,5000);
+                contador++;
+                NoRepeat(value.ToString());
+                BubbleSort(numbers);
+               
+            }
+            listBox1.Items.AddRange(numbers.ToArray());
+
+        }
+        static void NoRepeat(string item)
+        {
+
+            if (!numbers.Contains(item))
+            {
+
+                numbers.Add(item);
+            }
+            else
+            {
+               contador--;
+            }
+        }
+        static void BubbleSort(List<string> numeros)
+        {
+            for (int i = 0; i < numeros.Count; i++)
+            {
+                for (int k = i + 1; k < numeros.Count; k++)
+                {
+                    if (int.Parse(numeros[i]) > int.Parse(numeros[k]))
+                    {
+                        int temp = int.Parse(numeros[i]);
+                        numeros[i] = numeros[k];
+                        numeros[k] = temp.ToString();
+                    }
+                }
+            }
+        }
+
+        private void SearchLinear_Click(object sender, EventArgs e)
+        {
+            stopwatch.Restart();
+            int search = int.Parse(textBox1.Text) ;
+
+            int result = linearSearch(numbers, search.ToString());
+
+            if (result == -1)
+            {
+                MessageBox.Show("O número não existe na lista");
+                return;
+            }
+            stopwatch.Stop();
+            ts = stopwatch.Elapsed;
+            //Precisa ser resolvido
+            //string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",ts.TotalHours + "Horas", ts.TotalMinutes + "Minutos", ts.TotalSeconds + "Segundos", ts.TotalMilliseconds + "Milesimos");
+           
+
+            MessageBox.Show("O valor " + search + " está no índice " + result + " e isso levou " + ts);
+        }
+        public static int linearSearch(List<string> list, string value)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i] == value)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+        public static int BinarySearch(List<string> list,string valor)
+        {
+            stopwatch.Restart();
+            int min = 0;
+            int max = list.Count - 1;
+            while (max >= min)
+            {
+                
+                int mid = (min + max) / 2;
+
+               
+                if (int.Parse(list[mid]) < int.Parse(valor)) min = mid + 1;  
+
+                else if (int.Parse(list[mid]) > int.Parse(valor)) max = mid - 1;
+
+                else {
+                    stopwatch.Stop();
+                    MessageBox.Show(" O valor " + valor+ " foi encontrado na posição " + mid + " e isso levou " + stopwatch.Elapsed);
+                    return mid; 
+                } 
+            }
+            MessageBox.Show("O número não existe na lista");
+            return -1;
+        }
+        private void SearchBinary_Click(object sender, EventArgs e)
+        {
+            
+            int sercah = int.Parse(textBox1.Text);
+            BinarySearch(numbers,sercah.ToString());
+            
+        }
+    }
+}
